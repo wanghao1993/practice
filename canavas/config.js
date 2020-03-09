3aaf0f6e77ddba1781b187df5cb3210e797d24bf
汪浩 <hao.wang@huolala.cn>
2020-03-09 23:38:10 +0800

    canvas 表格

---
+++ canavas/config.js
@@ -0,0 +1,16 @@
+define('config', function () {
+    return {
+        ID: '',
+        data : {},
+        THEME: '',
+        startPosition: {
+            x: 0,
+            y: 0
+        },
+        boderWidth: 1,
+        columnHeight: 30,
+        columnWidth: 130,
+        width: 0,
+        height: 0
+    }
+})
--- canavas/index.js
+++ canavas/index.js
@@ -1,44 +1,94 @@
 
+
 (function () {
-    var DrawTable = function ({ID, DATA, THEME, BORDERWIDTH, ROWHEIGHT = 30, ROWWIDTH = 130, width = 800, height = 800}) {
-        this.ID = ID
-        this.THEME = THEME
-        this.BORDERWIDTH = BORDERWIDTH
-        this.ROWHEIGHT = ROWHEIGHT
-        this.ROWWIDTH = ROWWIDTH
-        this.canvas = document.getElementById(this.ID)
-        this.ctx = this.canvas.getContext('2d')
-        this.TABLEDATA = DATA
-        this.WIDTH = width
-        this.HEIGHT = height
-    }
-    DrawTable.prototype.constructor = DrawTable
-    DrawTable.prototype = {
-        init: function () {
-            this.canvas.width = this.WIDTH
-            this.canvas.height = this.HEIGHT
-            this.startDrawTableHeader()
-        },
-
-        startDrawTableHeader () {
-            let data = this.TABLEDATA.headers
-            const len = data.length
-            const ctx = this.ctx
-            console.log(100 + (len * this.ROWWIDTH))
-            ctx.beginPath()
-            ctx.moveTo(100, 20)
-            ctx.lineTo(100 + (len * this.ROWWIDTH), 20)
-            ctx.lineTo(100 + (len * this.ROWWIDTH), 20 + this.ROWHEIGHT)
-            ctx.lineTo(100, 20 + this.ROWHEIGHT)
-            ctx.lineTo(100, 20)
-
-            for(let i = 0; i < len; i++) {
-                ctx.moveTo(100 + (i * this.ROWWIDTH), 20)
-                ctx.lineTo(100 + (i * this.ROWWIDTH), 20 + this.ROWHEIGHT)
-            }
-            ctx.closePath()
-            ctx.stroke()
+    require(['config'], function(config){
+        config = config
+        var DrawTable = function (data) {
+            config = Object.assign(config, data)
+            this.ID = config.ID
+            this.startPosition = config.startPosition
+            this.THEME = config.THEME
+            this.BORDERWIDTH = config.BORDERWIDTH
+            this.columnHeight = config.columnHeight
+            this.columnWidth = config.columnWidth
+            this.canvas = {}
+            this.ctx = {}
+            this.tabledata = data
+            this.WIDTH = config.width || window.innerWidth
+            this.HEIGHT = config.height || window.innerHeight
         }
-    }
-    window.DrawTable = window.DrawTable || DrawTable
+        DrawTable.prototype.constructor = DrawTable
+        DrawTable.prototype = {
+            init: function () {
+                this.canvas = document.getElementById(this.ID)
+                this.ctx = this.canvas.getContext('2d')
+                this.canvas.width = this.WIDTH
+                this.canvas.height = this.HEIGHT
+                this.startDrawTableHeader()
+                this.startDrawTableBody()
+                window.onresize = function () {
+                    this.WIDTH = window.innerWidth
+                    this.HEIGHT = window.innerHeight
+                    this.init()
+                }.bind(this, )
+            },
+            // 绘制表头
+            startDrawTableHeader () {
+                let data = config.data.headers
+                const len = data.length
+                const ctx = this.ctx
+                const {startPosition} = config
+
+                // 填充背景色
+                // ctx.beginPath()
+                // ctx.fillStyle = '#fafafa'
+                // ctx.rect(this.startPosition.x, this.startPosition.y, this.columnWidth * len, this.columnHeight)
+                // ctx.closePath()
+                
+                ctx.beginPath()
+                ctx.moveTo(startPosition.x, startPosition.y)
+                ctx.lineTo(startPosition.x + (len * this.columnWidth), startPosition.y)
+                ctx.lineTo(startPosition.x + (len * this.columnWidth), startPosition.y + this.columnHeight)
+                ctx.lineTo(startPosition.x, startPosition.y + this.columnHeight)
+                ctx.font = 'bold 14px serif'
+                ctx.textAlign = "center";
+                // 设置垂直对齐方式
+                ctx.textBaseline = "middle";
+                for(let i = 0; i < len; i++) {
+                    ctx.fillText(data[i].label, startPosition.x + (i * this.columnWidth) + this.columnWidth / 2, startPosition.y + this.columnHeight / 2)
+                    ctx.moveTo(startPosition.x + (i * this.columnWidth), startPosition.y)
+                    ctx.lineTo(startPosition.x + (i * this.columnWidth), startPosition.y + this.columnHeight)
+                }
+                ctx.closePath()
+                ctx.stroke()
+
+            },
+
+            startDrawTableBody () {
+                const data = config.data.tableDatas || []
+                const headers = config.data.headers || []
+                const { ctx } = this
+                ctx.beginPath()
+
+                // ctx.lineTo(this.startPosition.x, this.startPosition.y + (data.length + 1) * this.columnHeight)
+                // ctx.lineTo(this.startPosition.x + headers.length * this.columnWidth, this.startPosition.y + (data.length + 1) * this.columnHeight)
+                ctx.rect(this.startPosition.x, this.startPosition.y + this.columnHeight, headers.length * this.columnWidth, data.length * this.columnHeight)
+                ctx.closePath()
+
+                ctx.font = 'bold 14px serif'
+                ctx.textAlign = "center";
+                // 设置垂直对齐方式
+                ctx.textBaseline = "middle";
+                headers.forEach((header, h_index) => {
+                    ctx.moveTo(this.startPosition.x + h_index * this.columnWidth, this.startPosition.y + data.length * this.columnHeight)
+                    
+                    // 绘制表格
+                })
+                ctx.stroke()
+            }
+        }
+
+        console.log(DrawTable)
+        window.DrawTable = window.DrawTable || DrawTable
+    })
 })(window)
